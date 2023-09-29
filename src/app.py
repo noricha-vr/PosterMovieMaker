7a4681682b00bb196a9ba2925b9c1ef7abecbe1e
noricha-vr <noricha.vr@gmail.com>
2023-09-29 08:06:55 +0000

    install extensions.

--- src/app.py
+++ src/app.py
@@ -5,19 +5,20 @@ from movie import image_to_movie, MovieConfig, download_images, upload_blob
 
 app = Flask(__name__)
 
+bucket_name = "TaAGatheringListSystem"
+source_file_name = "movie/output.mp4"
+destination_blob_name = "TaAGatheringList.mp4"
+json_url = 'https://noricha-vr.github.io/toGithubPagesJson/sample.json'
 
 @app.route('/')
 def run_script():
-    response = requests.get('https://noricha-vr.github.io/toGithubPagesJson/sample.json')
+    response = requests.get(json_url)
     data = json.loads(response.text)
     
     images = download_images(data)
     movie_config = MovieConfig(frame_rate='1', width='640', encode_speed='fast', output_movie_path='movie/output.mp4', image_type='png')
     image_to_movie(movie_config, images)
 
-    bucket_name = "TaAGatheringListSystem"
-    source_file_name = "movie/output.mp4"
-    destination_blob_name = "TaAGatheringList.mp4"
 
     upload_blob(bucket_name, source_file_name, destination_blob_name)
 
