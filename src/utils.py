264d1bab4fe22897a214cd08a8e609cbf1ba8a8b
noricha-vr <noricha.vr@gmail.com>
2023-09-29 16:33:05 +0000

    Pass download image test.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
+import pathlib
+import shutil
 import subprocess
+from threading import Thread
+from typing import List
 from google.cloud import storage
 import time
+import requests
+import os
+import logging
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(ch)
+
 class MovieConfig:
     def __init__(self, frame_rate, width, encode_speed, output_movie_path, image_type='png'):
         self.frame_rate = frame_rate
@@ -9,33 +22,85 @@ class MovieConfig:
         self.output_movie_path = output_movie_path
         self.image_type = image_type
 
+
+
 def download_images(data)->list:
     # ダウンロードした画像ファイルのパスを返す
     images = []
     default_image_url = "https://cdn.discordapp.com/attachments/1136630413054464070/1147808605660270642/10.png"
+    
+    # imgフォルダがない場合、作成する
+    if not os.path.exists("img"):
+        os.makedirs("img")
+    
     for i, item in enumerate(data):
         poster_url = item.get("ポスター", default_image_url)
         image_file_name = f"img/{i:03d}.png"
-        subprocess.run(f"wget {poster_url} -O {image_file_name}", shell=True)
-        images.append(image_file_name)
-    return image_file_name
+        
+        # requestsを使用して画像をダウンロード
+        try:
+            response = requests.get(poster_url, timeout=3)
+            response.raise_for_status()  # エラーレスポンスを確認し、エラーの場合は例外を発生させる
+            with open(image_file_name, 'wb') as f:
+                f.write(response.content)
+            images.append(image_file_name)
+        except requests.RequestException as e:
+            print(f"画像のダウンロードに失敗しました: {e}")
+    
+    return images  # images リストを返すように変更しました。
 
-def image_to_movie(movie_config: MovieConfig, image_paths: list):
+def copy_images_for_frame_rate(image_paths: List[str], frame_rate) -> None:
+    """
+    Copy files to create images for frame rate.
+    :param image_paths:
+    :param frame_rate:
+    :return:
+    """
+    # copy images framerate times -1
+    threads = []
+    for i in range(1, frame_rate):
+        for image_path in image_paths:
+            # copy image. ex) image.png -> image_01.png
+            new_image_path = image_path.replace(
+                f'.{image_path.split(".")[-1]}',
+                f'_{i:02d}.{image_path.split(".")[-1]}')
+            
+            t = Thread(target=shutil.copy,
+                            args=(
+                                image_path,
+                                new_image_path))
+            t.start()
+            threads.append(t)
+    [thread.join() for thread in threads]
+
+
+def image_to_movie(movie_config: MovieConfig,image_paths:List[str]) -> None:
+    """
+    Create image_dir files to movie.
+    :param movie_config:
+    :return None:
+    """
     if len(image_paths) == 0:
         raise Exception("No image files.")
-    
+    # copy images for frame rate
+    copy_images_for_frame_rate(image_paths, movie_config.frame_rate)
+    # stop watch
     start = time.time()
-    subprocess.call(['ffmpeg',
-                    '-framerate', f'{movie_config.frame_rate}',
-                    '-pattern_type', 'glob', '-i', f'img/*.{movie_config.image_type}',
-                    '-vf', f"scale='min({movie_config.width},iw)':-2",
-                    '-c:v', 'h264',
-                    '-pix_fmt', 'yuv420p',
-                    '-preset', movie_config.encode_speed,
-                    '-tune', 'stillimage',
-                    '-y',
-                    f'{movie_config.output_movie_path}'])
-    print(f"image_to_movie: {time.time() - start} sec")    
+    image_dir = pathlib.Path(image_paths[0]).parent + '.'+movie_config.image_type
+    commands = ['ffmpeg',
+                '-framerate', f'{movie_config.frame_rate}',
+                # Select image_dir/*.file_type
+                '-pattern_type', 'glob', '-i', image_dir,
+                '-vf', f"scale='min({movie_config.width},iw)':-2",  # iw is input width, -2 is auto height
+                '-c:v', 'h264',  # codec
+                '-pix_fmt', 'yuv420p',  # pixel format (color space)
+                '-preset', movie_config.encode_speed,
+                '-tune', 'stillimage',  # tune for still image
+                '-y',  # overwrite output file
+                f'{movie_config.output_movie_path}']
+    logger.info(f'command: {commands}')
+    subprocess.call(commands)
+    logger.info(f"MovieMaker.image_to_movie: {time.time() - start} sec")
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,33 +1,40 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from src.utils import download_images, image_to_movie, upload_blob, MovieConfig  
+from utils import download_images, image_to_movie, upload_blob, MovieConfig  
+from unittest.mock import patch
+from moviepy.editor import VideoFileClip
+import os
 
 class TestUtils(unittest.TestCase):
     
-    @patch("subprocess.run")
-    def test_download_images(self, mock_subprocess_run):
+    def test_download_images(self):
         data = [
             {"ポスター": "https://cdn.discordapp.com/attachments/1136630413054464070/1147808605660270642/10.png"},
             {"ポスター": "https://cdn.discordapp.com/attachments/1136630413054464070/1147808687201734736/3.png?ex=6515e064&is=65148ee4&hm=41f9e78af5f07d90fe5b5b8caebf28483c54e0dbfa521c21b50e4431b0f00dd3&"}
         ]
         result = download_images(data)
-        self.assertEqual(result, "img/001.png")
-        self.assertEqual(mock_subprocess_run.call_count, 2)
+        self.assertEqual(result, ['img/000.png', 'img/001.png'])
     
+
     @patch("subprocess.call")
-    def test_image_to_movie(self, mock_subprocess_call):
-        movie_config = MovieConfig(frame_rate=2, width=640, encode_speed="fast", output_movie_path="output.mp4")
+    def test_image_to_movie_creation_and_duration(self, mock_subprocess_call):
+        # MovieConfigオブジェクトの作成とimage_pathsの定義
+        movie_config = MovieConfig(frame_rate=2, width=640, encode_speed="fast", output_movie_path="movie/output.mp4")
         image_paths = ["img/000.png", "img/001.png"]
+
+        # image_to_movie関数の呼び出し
         image_to_movie(movie_config, image_paths)
-        mock_subprocess_call.assert_called_once_with([
-                    'ffmpeg', '-framerate', '2', '-pattern_type', 'glob', '-i', 'img/*.png',
-                    '-vf', "scale='min(640,iw)':-2", '-c:v', 'h264', '-pix_fmt', 'yuv420p',
-                    '-preset', 'fast', '-tune', 'stillimage', '-y', 'output.mp4'
-                ])
-        
-        with self.assertRaises(Exception) as context:
-            image_to_movie(movie_config, [])
-        self.assertEqual(str(context.exception), "No image files.")
+
+        # 動画ファイルが存在することを確認
+        self.assertTrue(os.path.exists(movie_config.output_movie_path), "動画ファイルが存在しません")
+
+        # 動画の長さが、画像の枚数とフレームレートに基づいて正しいことを確認
+        with VideoFileClip(movie_config.output_movie_path) as clip:
+            expected_duration = len(image_paths) / movie_config.frame_rate
+            self.assertEqual(clip.duration, expected_duration)
+
+        # テスト後に動画ファイルを削除
+        os.remove(movie_config.output_movie_path)
     
     @patch("google.cloud.storage.Client")
     def test_upload_blob(self, mock_storage_client):
